8250068f3a7803bd35a8b6da118d8e7196fda5c8
Minjungh63 <wcdi3737@gmail.com>
2021-07-11 02:11:26 +0900

    swipe 이동 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector2 direction;
+
+    private int desiredLane = 1; // 0: 왼쪽라인, 1: 중간라인, 2: 오른쪽 라인
+    public float laneDisance = 1.5f;// 라인 사이의 거리
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // 입력된 swipe에 따라 라인번호 결정
+        if (SwipeManager.swipeRight) // 만약 오른쪽으로 스와이프했다면
+        {
+            desiredLane++; // 라인번호++
+            if (desiredLane == 3) // 라인번호<=2
+                desiredLane = 2;
+        }
+        if (SwipeManager.swipeLeft) // 만약 왼쪽으로 스와이프했다면
+        {
+            desiredLane--; // 라인번호--
+            if (desiredLane == -1) // 라인번호>=0
+                desiredLane = 0;
+        }
+
+        // 라인번호에 따른 object 좌표값 설정 
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up; 
+        if (desiredLane == 0)
+            targetPosition += Vector3.left * laneDisance;
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * laneDisance;
+        
+        if (transform.position == targetPosition)
+            return;
+        Vector3 diff = targetPosition - transform.position; // 이동해야하는 좌표값 - 현재 좌표값
+        Vector3 moveDir = diff.normalized * 25 * Time.deltaTime;  // diff의 단위벡터 * 25 * Time.deltaTime
+        if (moveDir.sqrMagnitude < diff.magnitude)
+            controller.Move(moveDir); // moveDir로 object 이동
+        else
+            controller.Move(diff); // diff로 object 이동
+       
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);    
+    }
+}
---
+++ Assets/Scripts/SwipeManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeManager : MonoBehaviour
+{
+    public static bool tap, swipeLeft, swipeRight;
+    private bool isDraging = false; // 드래그하고 있으면 true
+    private Vector2 startTouch; // tap이 시작된 좌표값
+    private Vector2 swipeDelta; // 스와이프된 거리
+
+    private void Update()
+    {
+        tap = swipeLeft = swipeRight = false;
+
+        // Standalone Input 모듈: 컨트롤러/마우스 입력에 대해 동작하도록 설계됨.
+        #region Standalone Inputs 
+        if (Input.GetMouseButtonDown(0)) // 만약 마우스 버튼을 눌렀다면 (0: 마우스 왼쪽버튼)
+        {
+            tap = true;
+            isDraging = true;
+            startTouch = Input.mousePosition; // startTouch = 클릭한 마우스 좌표값
+        }
+        else if (Input.GetMouseButtonUp(0)) // 만약 마우스 버튼을 눌렀다 바로 뗐다면 (0: 마우스 왼쪽버튼)
+        {
+            isDraging = false;
+            Reset();
+        }
+        #endregion
+
+        #region Mobile Input
+        if (Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began) // 만약 터치가 시작되었으면
+            {
+                tap = true;
+                isDraging = true;
+                startTouch = Input.touches[0].position; // startTouch = 터치한 좌표값
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)// 만약 터치된 손가락이 스크린에서 떨어질 때|| 모바일폰을 귀에 갖다 대거나 touch tracking을 수행하지 않아야 할 경우면
+            {
+                isDraging = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        swipeDelta = Vector2.zero;
+        if (isDraging) // 만약 드래그하고 있다면
+        {
+            if (Input.touches.Length > 0) // 모바일일 경우
+                swipeDelta = Input.touches[0].position - startTouch;
+            else if (Input.GetMouseButton(0)) // 마우스 입력일 경우
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+        }
+        //Did we cross the distance?
+        if (swipeDelta.magnitude > 125) // 만약 스와이프된 거리가 일정값 이상이면
+        {
+            float x = swipeDelta.x;
+
+            if (x < 0)
+                swipeLeft = true; // swipeDelta.x<0이면 swipeLeft = true
+            else
+                swipeRight = true; // swipeDelta.x>0이면 swipeRight = true
+            Reset();
+        }
+     
+    } 
+    private void Reset()
+        {
+            startTouch = swipeDelta = Vector2.zero; // (0,0)으로 초기화
+            isDraging = false;
+        }
+}
