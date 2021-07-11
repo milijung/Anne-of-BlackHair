5cab85e5768623ea9350585e837ea90bc4da84c2
Minjungh63 <wcdi3737@gmail.com>
2021-07-11 18:11:00 +0900

    Button 생성

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("mainGame");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
