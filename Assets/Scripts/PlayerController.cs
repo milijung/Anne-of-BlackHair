eca7d7d9a00a25f8904c590dffa08959a8e58e09
Minjungh63 <wcdi3737@gmail.com>
2021-07-14 01:11:17 +0900

    Addscore

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -56,7 +56,6 @@ public class PlayerController : MonoBehaviour
         {
             gamePanel.SetActive(true); // 패널 보이기
             fadeSprite.SetActive(true);
-            StopCoroutine(GameManager.AddScore());
             Time.timeScale = 0; // 일시정지
         }
     }
