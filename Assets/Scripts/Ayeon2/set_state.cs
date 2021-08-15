be76649a3e1e5052d7e30db49d13b2cb2e192f6f
woozoo_zinc <your_zinc@ewhain.net>
2021-08-15 16:19:24 +0900

    ann_run 추가

---
+++ Assets/Scripts/Ayeon2/set_state.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class set_state : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+        
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.BLACK")) return;
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.BLACK1")) animator.SetInteger("State",0);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.BLACK2")) animator.SetInteger("State",1);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.BLACK3")) animator.SetInteger("State",2);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.BLACK4")) animator.SetInteger("State",3);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.BLEACH")) animator.SetInteger("State",4);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.GOLD")) animator.SetInteger("State",5);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.ORANGE")) animator.SetInteger("State",6);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.GRAPE")) animator.SetInteger("State",7);
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.RED")) animator.SetInteger("State",8);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
