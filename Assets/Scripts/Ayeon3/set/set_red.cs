4c6329e3908941b1fbba01382caec916088e48ff
woozoo_zinc <your_zinc@ewhain.net>
2021-08-19 03:24:56 +0900

    아연 - Ann animation FIN

--- Assets/Scripts/Ayeon3/set/set_red.cs
+++ Assets/Scripts/Ayeon3/set/set_red.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class set_red : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("RED",true);    
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        ;
-        //animator.SetBool("RED",false);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
