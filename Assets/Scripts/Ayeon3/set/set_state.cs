74cb2335dff198ea7391856ce36ef5ff1198b577
woozoo_zinc <your_zinc@ewhain.net>
2021-08-19 17:19:40 +0900

    아연 - bool BST 는 boost 하는 중일 때 true

--- Assets/Scripts/Ayeon3/set/set_state.cs
+++ Assets/Scripts/Ayeon3/set/set_state.cs
@@ -7,7 +7,7 @@ public class set_state : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("ITEM",false);     
+        animator.SetBool("ITEM",false);     animator.SetBool("BST",false); 
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
