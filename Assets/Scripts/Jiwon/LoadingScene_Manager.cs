29d028024e3f151f60172676af4eaa9c4d324e4f
Minjungh63 <wcdi3737@gmail.com>
2021-09-07 22:38:34 +0900

    Tip 수정

--- Assets/Scripts/Jiwon/LoadingScene_Manager.cs
+++ Assets/Scripts/Jiwon/LoadingScene_Manager.cs
@@ -37,8 +37,8 @@ public class LoadingScene_Manager : MonoBehaviour
     {
         tips[0] = new string[] {"Tip. 고라니는 터치해서","재울 수 있어요"};
         tips[1] = new string[] {"Tip. 강은 다리를 이용해서", "건널 수 있어요" };
-        tips[2] = new string[] {"Tip.양끝라인에 있으면","밤고양이와 부딪힐 확률이 줄어들어요" };
-        tips[3] = new string[] {"Tip.빨간머리로 염색하면", "사람들의 시야에 들어와도 괜찮아요" };
+        tips[2] = new string[] {"Tip.가장자리에 있으면","밤고양이와 부딪힐 확률이 줄어들어요" };
+        tips[3] = new string[] {"Tip.빨간머리로 염색하면", "사람들의 시야에 들어가도 괜찮아요" };
         tips[4] = new string[] {"Tip.과수원에 도착하면","열매 5개를 얻을 수 있어요" };
         tips[5] = new string[] {"Tip.낮에는 고양이들이 자고있으니","밟지않도록 조심해야해요" };
         tips[6] = new string[] {"Tip.망원경을 든 린드부인은","시야가 넓으니 주의하세요" };
